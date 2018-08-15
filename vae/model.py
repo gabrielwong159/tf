eb49de20c21d381bbaa0c6e2c6008d6268272046
Gabriel Wong <gabrielwong159@gmail.com>
2018-08-15 15:41:24 +0800

    Update VAE training and results

--- vae/model.py
+++ vae/model.py
@@ -5,28 +5,30 @@ import tensorflow.contrib.slim as slim
 class VAE(object):
     latent_size = 2
     
-    def __init__(self, batch_size):
-        self.x = tf.placeholder(tf.float32, [batch_size, 28, 28, 1])
+    def __init__(self):
+        self.x = tf.placeholder(tf.float32, [None, 28, 28, 1])
+        batch_size = tf.shape(self.x)[0]
         
-        z_mean, z_log_var = self.encoder_network()
+        z_mean, z_log_var = VAE.encoder_network(self.x)
         eps = tf.random_normal([batch_size, VAE.latent_size], mean=0., stddev=1e-2)
         z = z_mean + tf.exp(z_log_var) * eps
+        self.z = z
         
-        logits = self.decoder_network(z)
+        logits = VAE.decoder_network(z)
         self.out = tf.nn.sigmoid(logits)
 
         labels_flat = tf.reshape(self.x, [batch_size, -1])
         logits_flat = tf.reshape(logits, [batch_size, -1])
         
-#         image_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_flat, logits=logits_flat)
-#         image_loss = tf.reduce_sum(image_loss, axis=-1)
-
-        image_loss = tf.reduce_sum(tf.squared_difference(labels_flat, logits_flat), axis=-1)
+        image_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_flat, logits=logits_flat)
+        image_loss = tf.reduce_sum(image_loss, axis=-1)
+        # image_loss = tf.reduce_sum(tf.squared_difference(labels_flat, logits_flat), axis=-1)
+        
         kl_loss = -0.5 * tf.reduce_sum(1.0 + 2.0*z_log_var - tf.square(z_mean) - tf.exp(2.0*z_log_var), 1)
         self.loss = tf.reduce_mean(image_loss + kl_loss)
         
-    def encoder_network(self):
-        net = slim.conv2d(self.x, 64, [2, 2], scope='conv1')
+    def encoder_network(x):
+        net = slim.conv2d(x, 64, [2, 2], scope='conv1')
         net = slim.conv2d(net, 64, [2, 2], stride=2, scope='conv2')
         net = slim.conv2d(net, 64, [1, 1], scope='conv3')
         net = slim.conv2d(net, 64, [1, 1], scope='conv4')
@@ -38,7 +40,7 @@ class VAE(object):
         z_log_var = slim.fully_connected(net, VAE.latent_size, scope='z_log_var')
         return z_mean, z_log_var
 
-    def decoder_network(self, x):
+    def decoder_network(x):
         net = slim.fully_connected(x, 128, scope='fc1')
         net = slim.fully_connected(net, 128 * 14 * 14, scope='fc2')
 
--- vae/train.py
+++ vae/train.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from tqdm import tqdm, trange
@@ -6,33 +7,38 @@ from model import VAE
 
 mnist = input_data.read_data_sets('data/')
 
+n_epochs = 16
 batch_size = 64
-learning_rate = 5e-4
-num_iterations = 10_000
-model_path = 'model/vae/model'
+learning_rate = 1e-4
+model_path = 'model/vae_xent/model'
 
 
 def train():
-    model = VAE(batch_size)
+    model = VAE()
     optimizer = tf.train.AdamOptimizer(learning_rate)
     train_step = optimizer.minimize(model.loss)
     
     saver = tf.train.Saver()
     with tf.Session() as sess:
-        for i in trange(num_iterations):
-            sess.run(tf.global_variables_initializer())
+        sess.run(tf.global_variables_initializer())
+        x = mnist.train.images.reshape([-1, 28, 28, 1])
+        n_iters = int(np.ceil(len(x) / batch_size))
+        
+        for epoch in range(n_epochs):
+            for i in trange(n_iters, desc=f'Epoch {epoch}'):
+                _, loss = sess.run([train_step, model.loss], feed_dict={
+                    model.x: x[i::n_iters],
+                })
 
-            x, y = mnist.train.next_batch(batch_size)
-            _, loss = sess.run([train_step, model.loss], feed_dict={
-                model.x: x.reshape([-1, 28, 28, 1]),
-            })
+                if i % 100 == 0:
+                    tqdm.write(f'Step {i}, loss: {loss}')
 
-            if i % 10 == 0:
-                tqdm.write(f'Step {i}, loss: {loss}')
-            if i % 100 == 0:
-                saver.save(sess, model_path)
-    
-    print(saver.save(sess, model_path))
+            print(saver.save(sess, model_path))
+            
+            test_image = mnist.test.images[:1].reshape([-1, 28, 28, 1])
+            out = sess.run(model.out, feed_dict={model.x: test_image})
+            out_image = np.squeeze(out[0]) * 255
+            cv2.imwrite(f'samples/{epoch}_{int(loss)}.png', out_image)
 
     
 if __name__ == '__main__':
