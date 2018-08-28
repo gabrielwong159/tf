a81f313564e3376ca0136f769d37b618d790b1b2
Gabriel Wong <gabrielwong159@gmail.com>
2018-08-28 18:06:34 +0800

    Change model architecture and training method

--- gan/model.py
+++ gan/model.py
@@ -1,81 +1,85 @@
 # https://github.com/znxlwm/tensorflow-MNIST-GAN-DCGAN/blob/master/tensorflow_MNIST_DCGAN.py
 # https://github.com/ytakzk/Mnist-DCGAN-for-Tensorflow/blob/master/dcgan.ipynb
+# https://towardsdatascience.com/gan-introduction-and-implementation-part1-implement-a-simple-gan-in-tf-for-mnist-handwritten-de00a759ae5c
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
 
 class GAN(object):
-    def __init__(self, smooth=0.9):
+    def __init__(self):
         self.x = tf.placeholder(tf.float32, [None, 28, 28, 1])
         self.z = tf.placeholder(tf.float32, [None, 100])
         self.keep_prob = tf.placeholder(tf.float32)
 
-        g_logits = self.generator(self.z)
-        self.out = tf.nn.sigmoid(g_logits)
+        g = self.generator(self.z, self.keep_prob)
+        d_fake = self.discriminator(g, self.keep_prob)
+        d_real = self.discriminator(self.x, self.keep_prob)
 
-        d_logits_fake = self.discriminator(g_logits)
-        d_logits_real = self.discriminator(self.x)
-
-        self.g_loss = self.generator_loss(d_logits_fake)
-        self.d_loss = self.discriminator_loss(d_logits_fake, d_logits_real, smooth)
-
-        with tf.name_scope('summaries'):
-            tf.summary.scalar('g_loss', self.g_loss)
-            tf.summary.scalar('d_loss', self.d_loss)
+        self.g_loss = self.generator_loss(d_fake)
+        self.d_loss = self.discriminator_loss(d_fake, d_real)
+        self.inference = g
+        
+        tf.summary.histogram('g', g)
+        tf.summary.histogram('d_fake', d_fake)
+        tf.summary.histogram('d_real', d_real)
+        tf.summary.scalar('g_loss', self.g_loss)
+        tf.summary.scalar('d_loss', self.d_loss)
         self.summary = tf.summary.merge_all()
 
-    def generator(self, inputs):
+    def generator(self, inputs, keep_prob):
         with tf.variable_scope('generator'):
-            with slim.arg_scope([slim.fully_connected], activation_fn=tf.nn.leaky_relu):
-                net = slim.fully_connected(inputs, 1024, scope='fc1')
+            with slim.arg_scope([slim.fully_connected],
+                                activation_fn=tf.nn.leaky_relu):
+                net = slim.fully_connected(inputs, 7*7*256, scope='fc1')
                 net = slim.batch_norm(net, scope='bn1')
-                
-                net = slim.fully_connected(net, 7*7*128, scope='fc2')
-                net = slim.batch_norm(net, scope='bn2')
-                
-            net = tf.reshape(net, [-1, 7, 7, 128], name='reshape')
+
+            net = tf.reshape(net, [-1, 7, 7, 256], name='reshape')
+            net = slim.dropout(net, keep_prob, scope='dropout1')
 
             with slim.arg_scope([slim.conv2d_transpose],
                                 kernel_size=[5, 5],
                                 stride=2,
                                 activation_fn=tf.nn.leaky_relu):
+                net = slim.conv2d_transpose(net, 128, scope='conv2')
+                net = slim.batch_norm(net, scope='bn2')
+
                 net = slim.conv2d_transpose(net, 64, scope='conv3')
                 net = slim.batch_norm(net, scope='bn3')
-                
-                net = slim.conv2d_transpose(net, 32, scope='conv4')
-                net = slim.batch_norm(net, scope='bn4')
-                
-                net = slim.conv2d_transpose(net, 1, stride=1, activation_fn=None, scope='conv5')
+
+            net = slim.conv2d_transpose(net, 1, [5, 5], stride=1,
+                                        activation_fn=tf.nn.sigmoid, scope='conv4')
         return net
 
-    def discriminator(self, inputs):
+    def discriminator(self, inputs, keep_prob):
         with tf.variable_scope('discriminator', reuse=tf.AUTO_REUSE):
             with slim.arg_scope([slim.conv2d],
                                 kernel_size=[5, 5],
-                                stride=2,
                                 activation_fn=tf.nn.leaky_relu):
-                net = slim.conv2d(inputs, 128, scope='conv1')
+                net = slim.conv2d(inputs, 64, stride=2, scope='conv1')
+                net = slim.dropout(net, keep_prob, scope='dropout1')
                 
-                net = slim.conv2d(net, 256, scope='conv2')
-                net = slim.batch_norm(net, scope='bn2')
+                net = slim.conv2d(net, 64, scope='conv2')
+                net = slim.dropout(net, keep_prob, scope='dropout2')
                 
-                net = slim.conv2d(net, 512, scope='conv3')
-                net = slim.batch_norm(net, scope='bn3')
-                
-                net = slim.conv2d(net, 1024, scope='conv4')
-                net = slim.batch_norm(net, scope='bn4')
-                
-                net = slim.conv2d(net, 1, stride=1, activation_fn=None, scope='conv5')
+                net = slim.conv2d(net, 64, scope='conv3')
+                net = slim.dropout(net, keep_prob, scope='dropout3')
+            
+            net = slim.flatten(net, scope='flat')
+            net = slim.fully_connected(net, 1, activation_fn=tf.nn.sigmoid, scope='fc5')
             return net
-    
+        
+    def binary_cross_entropy(self, labels, logits, eps=1e-12):
+        z, x = labels, logits
+        return z * -tf.log(x + eps) + (1. - z) * -tf.log(1. - x + eps)
+
     def generator_loss(self, logits_fake):
-        loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(logits_fake), logits=logits_fake)
+        loss = self.binary_cross_entropy(labels=tf.ones_like(logits_fake), logits=logits_fake)
         return tf.reduce_mean(loss)
 
-    def discriminator_loss(self, logits_fake, logits_real, smooth):
+    def discriminator_loss(self, logits_fake, logits_real):
         labels_fake = tf.zeros_like(logits_fake)
-        labels_real = tf.ones_like(logits_real) * (1 - smooth)
+        labels_real = tf.ones_like(logits_real)
 
-        loss_fake = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_fake, logits=logits_fake)
-        loss_real = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_real, logits=logits_real)
-        return tf.reduce_mean(loss_fake + loss_real)
+        loss_fake = self.binary_cross_entropy(labels=labels_fake, logits=logits_fake)
+        loss_real = self.binary_cross_entropy(labels=labels_real, logits=logits_real)
+        return tf.reduce_mean(0.5 * (loss_fake + loss_real))
--- gan/train.py
+++ gan/train.py
@@ -1,49 +1,62 @@
 import numpy as np
+import cv2
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from tensorflow.examples.tutorials.mnist import input_data
 from model import GAN
-from tqdm import tqdm, trange
+from tqdm import trange
 
-mnist = input_data.read_data_sets('data/', one_hot=True)
+mnist = input_data.read_data_sets('data/', reshape=False)
 
-g_lr = 2e-4
-d_lr = 1e-4
 batch_size = 128
-n_epochs = 20
+n_epochs = 10
 model_path = 'model/gan/model'
 
+# not ok
+# 2. having separate var list
+# 3. binary cross entropy with eps
 
+# maybe ok
+# 1. control dependencies <<<
+
+# ok
+# 1. batch norm decay
+# 2. regularization
+# 3. dimensionality of input noise
+# 4. bn after lrelu
+# 5. run both train steps on the same inputs
+            
 def train():
     model = GAN()
-    g_train_step = tf.train.AdamOptimizer(g_lr).minimize(model.g_loss, var_list=slim.get_variables(scope='generator'))
-    d_train_step = tf.train.AdamOptimizer(d_lr).minimize(model.d_loss, var_list=slim.get_variables(scope='discriminator'))
+    
+    vars_d = slim.get_variables(scope='discriminator')
+    d_optimizer = tf.train.RMSPropOptimizer(8e-4, decay=6e-8)
+    d_train_step = d_optimizer.minimize(model.d_loss, var_list=vars_d)
 
+    vars_g = slim.get_variables(scope='generator')
+    g_optimizer = tf.train.RMSPropOptimizer(4e-4, decay=3e-8)
+    g_train_step = g_optimizer.minimize(model.g_loss, var_list=vars_g)
+    
     saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         saver.restore(sess, model_path)
+        for epoch in trange(n_epochs):
+            writer = tf.summary.FileWriter(f'summaries/{epoch}', sess.graph)
+            
+            n_iters = int(np.ceil(len(mnist.train.labels) / batch_size))
+            for i in trange(n_iters, desc=f'Epoch {epoch}', leave=False):
+                x, _ = mnist.train.next_batch(batch_size)
+                z = np.random.uniform(-1, 1, size=[batch_size, 100])
 
-        for epoch in range(n_epochs):
-            summary_writer = tf.summary.FileWriter(f'summaries/{epoch}', sess.graph)
-            n_iters = int(np.ceil(len(mnist.train.images) / batch_size))
-            for i in trange(n_iters, desc=f'Epoch {epoch}'):
-                x, y = mnist.train.next_batch(batch_size)
-                x = x.reshape([-1, 28, 28, 1])
-                x = x*2 - 1
-                x += np.random.normal(-1, 1, size=x.shape)
-
-                z = np.random.normal(-1, 1, size=[batch_size, 100])
-
-                _, _, summary = sess.run([g_train_step, d_train_step, model.summary], feed_dict={
+                _, _, summary = sess.run([d_train_step, g_train_step, model.summary], feed_dict={
                     model.x: x,
                     model.z: z,
                     model.keep_prob: 0.5,
                 })
-                summary_writer.add_summary(summary, epoch*n_iters + i)
+                writer.add_summary(summary, epoch*n_iters + i)
             saver.save(sess, model_path)
-
+            
 
 if __name__ == '__main__':
     train()
-
